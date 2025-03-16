d618da006e103c0684ee98d3ca351387ea676f68
Adrian Osorio Blanchard <osorioadrian04@gmail.com>
2025-03-16 14:02:29 -0400

    pls man

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-      caches.open('portfolio-cache-v1').then((cache) => {
-          return cache.addAll([
-              '/',
-              '/index.html',
-              '/assets/style.css'
-          ]);
-      })
+    caches.open('portfolio-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/style.css'
+      ]);
+    })
   );
 });
 
@@ -18,13 +18,13 @@ self.addEventListener('fetch', (event) => {
     return fetch(event.request);
   }
   event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-          return cachedResponse || fetch(event.request).then((response) => {
-              return caches.open('portfolio-cache-v1').then((cache) => {
-                  cache.put(event.request, response.clone());
-                  return response;
-              });
-          });
-      })
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((response) => {
+        return caches.open('portfolio-cache-v1').then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
   );
 });
